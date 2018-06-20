3d1a1038b9dbd2c7a2c8f3acafa7fa3c0f5e4cea
ofrihosh <37741919+ofrihosh@users.noreply.github.com>
2018-06-20 16:35:22 +0300

    Add files via upload

---
+++ web site/jScripts/JavaScript.js
@@ -0,0 +1,31 @@
+﻿function show_hide(element) {
+    var con = element.parentElement.querySelector("#personal");
+    if (con.style.display == "") {
+        con.style.display = "block";
+    } else {
+        con.style.display = "";
+    }
+    element.classList.add('open');
+}
+
+
+function hide1() {
+    var X = document.querySelector("#personal");
+    X.style.display = "";
+}
+
+function show_hide2(element) {
+    var con = element.parentElement.querySelector("#groups");
+    if (con.style.display == "") {
+        con.style.display = "block";
+    } else {
+        con.style.display = "";
+    }
+    element.classList.add('open');
+}
+
+
+function hide2() {
+    var X = document.querySelector("#groups");
+    X.style.display = "";
+}
